06025194b35c303b00ee2f4776e6eb1133078f92
Yuri Salgado <yuri_salgadinho@hotmail.com>
2018-03-27 18:05:35 -0300

    finish tuto

---
+++ geofences/test/system/geofences_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class GeofencesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit geofences_url
+  #
+  #   assert_selector "h1", text: "Geofence"
+  # end
+end
